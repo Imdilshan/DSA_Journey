ee70f8d5d9bc2e7bbef065282acd9c6676d9faf2
Md Dilshan <imdilshan@proton.me>
2026-03-04 23:00:38 +0530

    Create a.cpp

---
+++ Contests/Codechef/Starters 228/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	
+	if(x > y) cout << "Alice";
+	else if(y > x) cout << "Bob";
+	else cout << "Draw" << endl;
+
+}
