e75d0a373178d6e850db8cbbb05ea4645da30db3
Md Dilshan <imdilshan@proton.me>
2026-03-26 15:27:38 +0530

    Create Implement Lower Bound.cpp

---
+++ Binary Search/Implement Lower Bound.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        int l = 0, r = arr.size() - 1;
+        
+        while( l <= r){
+            int mid = l + (r-l) / 2;
+            
+            if (arr[mid]>= target){
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
